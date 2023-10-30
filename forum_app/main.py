3e60e259d25876badb375756fd6b95d83d812c96
Yasen Sotirov <108167481+ysotirov@users.noreply.github.com>
2023-10-30 11:14:16 +0200

    Update main.py

--- forum_app/main.py
+++ forum_app/main.py
@@ -3,14 +3,14 @@ from routers.users import users_router
 from routers.messages import messages_router
 from routers.categories import categories_router
 from routers.topics import topics_router
-
+from routers.replies import replies_router
 
 app = FastAPI(title='The Sport Forum', description='Team #5 web project on Telerik Academy')
 app.include_router(users_router)
 app.include_router(messages_router)
 app.include_router(categories_router)
 app.include_router(topics_router)
-
+app.include_router(replies_router)
 
 
 if __name__ == "__main__":
