3cc67b7489d7d76660d556c77a8a17dea340a969
Yasen Sotirov <108167481+ysotirov@users.noreply.github.com>
2023-10-30 11:09:59 +0200

    Update replies.py

--- forum_app/routers/replies.py
+++ forum_app/routers/replies.py
@@ -1,28 +1,41 @@
-from data.database import read_query, insert_query
-from services import topic_service
+from fastapi import APIRouter, Header
 from fastapi.responses import JSONResponse
+from authentication.authenticator import get_user_or_raise_401
+from services import reply_services, topic_service, user_service
+
+
+replies_router = APIRouter(prefix='/replies', tags=['Replies'])
+
+
+@replies_router.get('/{title}')
+def view_replies_by_topic_title(topic_title: str, x_token: str = Header()):
+    get_user_or_raise_401(x_token)
+    if not topic_service.topic_exists(topic_title):
+        return JSONResponse(status_code=404, content=f'No topic with name "{topic_title}"')
+
+    topic_id  = topic_service.find_topic_id_by_name(topic_title)
+    replies = reply_services.read_replies_by_topic_id(topic_id)
+
+    result = []
+    for data in replies:
+        username = user_service.find_user_by_id(data[1])
+        data_dict = {"reply text": data[0], "reply by": username}
+        result.append(data_dict)
+    return result
 
 
 
-def read_replies_by_topic_id(topic_id):
-    data = read_query('''SELECT text, new_user_id_of_user 
-                        FROM replies WHERE  new_topic_id_of_topic = ?''',
-                      (topic_id,))
-    return data
-
-
-
-def create_reply(topic_title: str, text: str, username):
-    get_author_id = topic_service.find_id_nickname(username)
-    author_id = get_author_id[0][0]
-    topic_id = topic_service.find_topic_id_by_name(topic_title)
+@replies_router.post('/add_reply')
+def add_reply(topic_title: str, reply_text:str, username, x_token: str = Header()):
+    get_user_or_raise_401(x_token)
 
     if not topic_service.topic_exists(topic_title):
-        return JSONResponse(status_code=404, content='There is no such topic')
+        return JSONResponse(status_code=404, content=f'No topic with name "{topic_title}"')
 
-    insert_query('''INSERT INTO replies (text, new_topic_id_of_topic, new_user_id_of_user)
-                    VALUES (?, ?, ?)''', (text, topic_id, author_id))
+    return reply_services.create_reply(topic_title, reply_text, username)
 
-    return 'The reply was added successfully'
+
+
+
 
 
